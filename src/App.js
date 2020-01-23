b9c337ee48622af22ffe1d45e0f4485416b77013
David Behar <davbelom@gmail.com>
2020-01-23 12:52:20 -0600

    Logo for the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Home from './pages/Home';
 
 import GlobalStyles from './styles/globalStyles';
+
+import Home from './pages/Home';
+import Logo from './components/Logo';
 
 function App() {
   return (
 
     <div className="App">
       <GlobalStyles />
+      <Logo />
       <Home />
     </div>
   );
