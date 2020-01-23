dc08bd3c8c98c02c2092dff720f6134def000d48
David Behar <davbelom@gmail.com>
2020-01-23 12:05:06 -0600

    ToggleLikeMutation

---
+++ src/apollo/ToggleLikeMutation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+const LIKE_PHOTO = gql`
+mutation likeAnonymousPhoto($input: LikePhoto!) {
+  likeAnonymousPhoto(input: $input) {
+    id,
+    liked,
+    likes
+  }
+}
+`;
+
+const ToggleLikeMutation = (props) => {
+  const { children } = props;
+  return (
+    <Mutation mutation={LIKE_PHOTO}>
+      {children}
+    </Mutation>
+  );
+};
+
+export default ToggleLikeMutation;
