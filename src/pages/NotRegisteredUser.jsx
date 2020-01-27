e02dc62706699ea7bee9d079342bef77f2a6660b
David Behar <davbelom@gmail.com>
2020-01-27 08:09:22 -0600

    Usar useContext para simplificar la lógica del login

--- src/pages/NotRegisteredUser.jsx
+++ src/pages/NotRegisteredUser.jsx
@@ -1,66 +1,53 @@
-import React from 'react';
-import Context from '../Context';
+import React, { useContext } from 'react';
+import { Context } from '../Context';
 import UserForm from '../components/UserForm';
 
 import RegisterMutation from '../apollo/RegisterMutation';
 import LoginMutation from '../apollo/LoginMutation';
 
 const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context);
+
   return (
-    <Context.Consumer>
-      {
-        ({ activateAuth }) => {
-          return (
-            <>
-              <RegisterMutation>
-                {
-                  (register, { loading, data, error }) => {
-                    const onSubmit = ({ email, password }) => {
-                      const input = { email, password };
-                      const variables = { input };
-                      register({ variables })
-                        .then(
-                          (data) => {
-                            const { signup } = data;
-                            activateAuth(signup);
-                          },
-                        )
-                        .catch((error) => { console.log(error.graphQLErrors[0].message) ; });
-                    };
+    <>
+      <RegisterMutation>
+        {
+          (register, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password };
+              const variables = { input };
+              register({ variables }).then(({ data }) => {
+                const { signup } = data;
+                activateAuth(signup);
+              });
+            };
 
-                    const errorMsg = error && error.graphQLErrors[0].message;
-                    console.log(errorMsg);
-                    return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title="Registrarse" />;
-                  }
-                }
-              </RegisterMutation>
-              <LoginMutation>
-                {
-                  (login, { loading, data, error }) => {
-                    const onSubmit = ({ email, password }) => {
-                      const input = { email, password };
-                      const variables = { input };
-                      login({ variables })
-                        .then(
-                          ({ data }) => {
-                            const { login } = data;
-                            activateAuth(login);
-                          },
-                        )
-                        .catch((error) => { console.log(error.graphQLErrors[0].message) ; });
-                    };
+            const errorMsg = error && 'El usuario ya existe o hay algún problema.';
 
-                    const errorMsg = error && error.graphQLErrors[0].message;
-                    console.log(errorMsg);
-                    return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title="Ingresar" />;
-                  }
-                }
-              </LoginMutation>
-            </>
-          );
+            return <UserForm disabled={loading} error={errorMsg} title="Registrarse" onSubmit={onSubmit} />;
+          }
         }
-      }
-    </Context.Consumer>
+      </RegisterMutation>
+
+      <LoginMutation>
+        {
+          (login, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password };
+              const variables = { input };
+              login({ variables }).then(({ data }) => {
+                const { login } = data;
+                activateAuth(login);
+              });
+            };
+
+            const errorMsg = error && 'La contraseña no es correcta o el usuario no existe';
+
+            return <UserForm disabled={loading} error={errorMsg} title="Iniciar sesión" onSubmit={onSubmit} />;
+          }
+        }
+      </LoginMutation>
+    </>
   );
 };
 
